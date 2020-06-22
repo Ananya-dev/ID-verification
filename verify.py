213c30aff2dce0a580b91e06f85a9a83f85f6946
Ananya-dev <64248612+Ananya-dev@users.noreply.github.com>
2020-06-22 16:25:10 +0530

    verify.py

---
+++ verify.py
@@ -0,0 +1,37 @@
+import os
+from verifai_sdk import VerifaiService
+
+
+# First we setup the class
+service = VerifaiService(token='<INSERT YOUR API TOKEN HERE>')
+service.add_clasifier_url('http://localhost:5000/api/classify/')
+
+# Error for non existing
+service.add_clasifier_url(
+    'http://localhost:6000/api/classify/',
+    skip_unreachable=True
+)
+
+# By path to the JPEG
+sample_dir = 'docs/sample_images/'
+image_path = os.path.join(sample_dir, 'dutch-id-front-sample.jpg')
+document, confidence = service.classify_image_path(image_path)
+print(document)
+print("Classified as", document.id_uuid)
+print("With confidence level: ", confidence)
+
+print(document.model)
+print(document.country)
+print(document.position_in_image)
+
+print(document.zones)
+print(document.zones[0].title)
+print(document.zones[0].side)
+
+# ID Front + masking of all zones
+document, confidence = service.classify_image_path('docs/sample_images/dutch-id-front-sample.jpg')
+
+masked_image = document.mask_zones(document.zones)
+
+# Save it
+masked_image.save('masked.jpg')
